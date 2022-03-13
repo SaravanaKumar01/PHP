5472d8f2b2ed3756f889f43cd1422a5b62b2b7de
Saravana Kumar <93923882+SaravanaKumar01@users.noreply.github.com>
2022-03-13 20:58:29 +0100

    Add files via upload

---
+++ style.php
@@ -0,0 +1,147 @@
+<?php
+Header("content-type:text/css");
+?>
+*{margin:0; padding:0;outline:0;}
+body{font-family:arial; font-size:14px; line-height:18px;color:#000;}
+
+.templete{   width: 960px;; background-color:#ddd; margin:0 auto;}
+.clear{overflow:hidden;}
+.header  {height:90px; background-color:#86CCED;color:#fff;}
+.header h1{    font-size: 40px;
+    text-align: center;
+    padding-top: 25px;}
+.logo{    float: left;
+    width: 500px;}
+.logo img {    width: 80px;
+    padding: 5px 15px;    float: left;}
+.logo p {    color: green;
+    font-size: 20px;
+    font-weight: bold;    padding: 10px;}
+.social {    float: right;}
+.social a{}
+.social img {    width: 30px;
+    margin: 5px;
+    padding: 14px;}
+.social img:hover{background:white;}
+.notis{}
+.td1{width: 50px;color:green;background-color:white;}
+.td2{width: 100%;color:white;background-color:black;}
+.nav{       height: 40px;
+    color: #fff;
+    text-align: center;
+    font-size: 25px;
+    font-weight: bold;
+    background-color: #028600a6;}
+.nav ul{margin: 0;
+    padding: 0;
+    list-style: none;}
+.nav li{       float: left;
+    text-decoration: none;
+    padding: 12px 10px;
+    font-size: 16px;
+    text-align: center;}
+.nav a{    color: white;
+    background: black;
+    text-decoration: none;
+    border-radius: 20px;
+    padding: 5px;
+    border: 2px solid red}
+.nav ul li a:hover{background:white ;color:black;}
+.ma{background-color:#ffffff;}
+.con{    width: 648px;
+    background-color: #AD8BFE;
+    float: left;
+        margin: 15px 5px;}
+.con p {}
+.conl{ 
+    background-color: #1c3b4fd9;
+	
+    color: white;
+	line-height: 30px;
+    font-weight: bolder;
+    text-align: -webkit-center;
+    padding-top: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;     }
+.conl content{width: 30%;}
+.sid{    width: 200px;
+    background: #33FDC0;
+    float: right;
+   
+    margin-top: 15px;
+    margin-right: 200px;
+    margin-bottom: 10px;
+    margin-left: 0px;}
+.back{background-image: url(st/1.jpg);}
+.back1{background-image: url(st.jpg);}
+.conl a{background-color: #ffffffad;}
+.error{
+		color:red;
+	}
+.d{width:500px; margin:0 auto;
+}
+.conl1{    background-color: #ffffff8c;}
+.conl1 tr{    line-height: 42px;    outline: 2px solid cornflowerblue;}
+.conl1 table {    width: 100%;
+    padding: 13px;}
+	
+.con2{    background-color: #ffffff8c;}
+.con2 tr{    line-height: 42px;    outline: 2px solid cornflowerblue;}
+.con2 td{    border: 1px solid blue;
+    text-align: center;}
+.con2 table {    width: 100%;
+    padding: 13px;}
+
+.fot{       height: 170px;
+    background-color: #CCFFCC;
+    color: seagreen;
+    font-weight: bolder;
+    text-align: -webkit-center;
+    }
+.fot1{    padding-left: 258px;}
+.fot1 ul {margin:0;padding:0;list-style:none}
+.fot1 li{color: #fff;
+    text-align: center;
+    font-size: 25px;
+	font-weight: bold;
+	float: left;}
+.fot1 li a{display: block;
+    background: snow;
+    text-decoration: none;
+    padding: 5px 11px;
+    margin: 5px;
+    color: black;
+    border-radius: 10px;}
+	
+.fot1 ul li a:hover{background:black;color:white;}
+.fot2 {}
+.f1{      float: left;
+    width: 250px;
+    padding-right: 35px;
+    padding: 0px 40px;
+}
+.f1 img{    width: 50px;}
+.f1 p {}
+.f2{ float: left;
+    width: 250px;  padding: 8px 25px;}
+.f2 img{    width: 50px;}
+.f2 p {}
+.f3{ float: right;;
+    width: 250px;        padding: 15px;;}
+.f3 img{    width: 50px;}
+.f3 p{}
+.f4{     background: black;
+    width: 960px;
+    height: 35px;
+    float: left;
+    padding-top: 15px;}
+
+.edit {    background: blue;
+    color: white;    padding: 10px 10px;font-weight: bold;
+    border-radius: 15px;}
+.edit:hover{ background: white;color: blue; }
+.delete{  background: red;font-weight: bold;
+    color: white;    padding: 10px 10px;
+    border-radius: 15px;}
+.delete:hover{ background: white;color: red; }
---
+++ style1.css.php
@@ -0,0 +1,52 @@
+<?php
+Header("content-type:text/css");
+?>
+*{margin:0; padding:0;outline:0;}
+body{font-family:arial; font-size:14px; line-height:18px;color:#000;}
+
+.templete{ width:960px; margin:0 auto;}
+.clear{overflow:hidden;}
+.con{min-height: 660px;
+    line-height: 22px;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    text-align: justify;}
+.con h1{     text-align: center;
+    margin-bottom: 50px;  }
+	
+.con p{  }
+.con table{margin: 0 auto;line-height: 25px;    font-weight: bold;}
+.con table tr{}
+.con table tr td{    text-align: center;
+    border: 2px solid #5a53538a;
+    padding-right: 5px;
+    color: white;
+	line-height: 40px;}
+.back{background-color: #3a3a3a; }
+.error{
+		color:red;
+	}
+.edit {    background: blue;
+    color: white;    padding: 5px 5px;font-weight: bold;
+    border-radius: 15px;}
+.edit:hover{ background: white;color: blue; }
+.delete{  background: red;font-weight: bold;
+    color: white;    padding:5px 5px;
+    border-radius: 15px;}
+.delete:hover{ background: white;color: red; }
+
+.pop1{    width: 100%;
+    height: 10;
+    height: 100%;
+    background-color: #15141461;
+    position: absolute;
+    top: 0;display:flex;justify-content:center;align-items:center;}
+.pop2{
+	width: 200px;
+    height: 80px;
+    background-color: white;
+	text-align:center;
+}
+.re table tr td input{width: 50px;
+    border: 2px solid green;}
+.m{}
